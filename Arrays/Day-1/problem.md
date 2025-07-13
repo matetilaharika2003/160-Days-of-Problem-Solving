6f708771937bababb207911400ebc38ccb0e20b9
laharika mateti <bindhumateti123@gmail.com>
2025-07-13 19:15:31 +0530

    Day 1: Solved SecondLargest Element in Array

---
+++ Arrays/Day-1/problem.md
@@ -0,0 +1,23 @@
+Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
+
+Note: If the second largest element does not exist, return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+Input: arr[] = [10, 10, 10]
+Output: -1
+Explanation: The largest element of the array is 10 and the second largest element does not exist.
+
+Constraints:
+2 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
---
+++ Arrays/Day-1/solution.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Solution {
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        int largest = -1, secondLargest = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+
+            } else if (arr[i] < largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+
+            }
+        }
+        return secondLargest;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if (n < 2) {
+            System.out.println("-1");
+            return;
+
+        }
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+
+        System.out.println(getSecondLargest(arr));
+    }
+}
